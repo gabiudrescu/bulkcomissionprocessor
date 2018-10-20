43c7a61e288ba23dbb0d1096269cc32683ad8a6d
ana-mihaela <ana.mihaela.v@gmail.com>
2018-10-20 15:48:46 +0300

    create angular project

--- gui/bulkcomision-ui/src/app/feature/feature.module.ts
+++ gui/bulkcomision-ui/src/app/feature/feature.module.ts
@@ -4,11 +4,13 @@ import { RouterModule } from '@angular/router';
 import { routes } from './feature.routes';
 import { FeatureComponent } from './feature.component';
 import { HomeComponent } from './home/home.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 @NgModule({
   declarations: [
     FeatureComponent,
-    HomeComponent
+    HomeComponent,
+    TasksComponent
   ],
   imports: [
     RouterModule.forChild(routes),
--- gui/bulkcomision-ui/src/app/feature/feature.routes.ts
+++ gui/bulkcomision-ui/src/app/feature/feature.routes.ts
@@ -1,10 +1,12 @@
 import { FeatureComponent } from './feature.component';
 import { HomeComponent } from './home/home.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 export const routes = [
   { path: '', children: [
     { path: '', component: FeatureComponent, children: [
         { path: '', component: HomeComponent },
+        { path: 'tasks', component: TasksComponent },
       ]}
   ]}
 ];
